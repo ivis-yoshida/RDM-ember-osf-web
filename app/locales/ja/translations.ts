04643969f30e19ce14245912dbcede5f8631f7a8
west1636 <nishikawa@nii.ac.jp>
2020-03-26 17:06:52 +0900

    revise GakuNin RDM (Test/Trial) for ja translations.ts

--- app/locales/ja/translations.ts
+++ app/locales/ja/translations.ts
@@ -1,4 +1,20 @@
 /* tslint:disable:object-literal-sort-keys max-line-length */
+import config from 'ember-get-config';
+
+const {
+    organization,
+    OSF: {
+        pageName,
+	longBrand,
+    },
+    signUpPolicy: {
+        privacyPolicyLink,
+        cookiesLink,
+    },
+    navbar: {
+        useSearch,
+    },
+} = config;
 
 export default {
     documentType: {
