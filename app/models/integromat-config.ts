1c00fb46eae30d0598f505fcd5c3960f84b5b66c
west1636 <nishikawa@nii.ac.jp>
2020-11-16 12:20:01 +0900

    integromat config(4)

--- app/models/integromat-config.ts
+++ app/models/integromat-config.ts
@@ -2,8 +2,6 @@ import { attr } from '@ember-decorators/data';
 import DS from 'ember-data';
 import OsfModel from './osf-model';
 
-const { attr } = DS;
-
 export default class IntegromatConfigModel extends OsfModel {
     @attr('string') webhook_url!: string;
 }
