241d673603d6bfbab9628494a436ca1d44d17b58
ivis-obayashi <takeru.obayashi@ivis.co.jp>
2020-12-25 18:45:55 +0900

    誤記の修正

--- app/models/dmp-status.ts
+++ app/models/dmp-status.ts
@@ -4,17 +4,11 @@ import OsfModel from './osf-model';
 const { attr } = DS;
 
 export default class DMPStatusModel extends OsfModel {
-    @attr('string') dmp_id!: string;
-    @attr('string') contact!: string;
-    @attr('string') project!: string;
-    @attr('string') dataset!: string;
-    @attr('string') created!: string;
-    @attr('string') modified!: string;
         
-//    @attr('string') name!: string;
-//    @attr('string') mbox!: string;
-//    @attr('string') title!: string;
-//    @attr('string') description!: string;
+    @attr('string') name!: string;
+    @attr('string') mbox!: string;
+    @attr('string') title!: string;
+    @attr('string') description!: string;
 }
 
 declare module 'ember-data/types/registries/model' {
